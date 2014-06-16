30363d6b71d2b43d346f3a61b28eb2e661ecb9c9
Bryan <bpoteryko@gmail.com>
2014-06-16 14:47:31 -0700

    Heeeeeere's Zach!
    Zach's lovely data.

---
+++ Arduino Code/arduinoTemperatureTest/arduinoOff.m
@@ -0,0 +1,4 @@
+display('Turning off');
+
+a.pinMode(2,'output');
+a.digitalWrite(2,1);
--- Arduino Code/arduinoTemperatureTest/temperatureReaderMarkIV.m
+++ Arduino Code/arduinoTemperatureTest/temperatureReaderMarkIV.m
@@ -17,11 +17,11 @@ hour = minute*60;
 % Value, in seconds, we want to run the program for. Thanks to the above
 % constants, we can simply enter in the time period in the time units we
 % prefer.
-period = 20*minute;
+period = 3*hour;
 
 % Our lovely constants
 to_voltage = (5.0 / 1023.0); % Changes analog values to their equivalent voltages.
-gain = 805.0;                % Gain of the op-amp. This has been changed from previous versions of the circuit.
+gain = 247.0;                % Gain of the op-amp. This has been changed from previous versions of the circuit.
 carley_couple = 24.7*1000.0; % Carley's Thermocouple Constant, converts temperature values.
 offset = 0.972;              % The voltage offset we introduced in the circuit.
 
@@ -36,6 +36,7 @@ disp('Begun Recording');
 % h = waitbar(0, 'Recording Data...');
 
 for i = 1:period
+    tic;
     % First, we initialize our sensorValue vector. This will store all our
     % data for this time step.
     sensorValue = zeros(1,6);
@@ -64,10 +65,12 @@ for i = 1:period
     
     % Thermocouple 3 is really odd, it consistently plots data at 5 degrees
     % below what it actually reads. Hence, we're correcting it here.
-    T(4,i) = T(4,i) + 3.5;
+    T(2,i) = T(2,i) - 1.5; % Thermocouple 1
+%     T(4,i) = T(4,i) + 3.5; % Thermocouple 3
+    T(5,i) = T(5,i) + 1.5; % Thermocouple 4
 
     % This sets up our x-axis, and plots the data.
-    x = 1:dT:i;    
+    x = 1:dT:i;
     plot(x, T(1:6,1:i));
 
     % As a safety measure, we save the variables to file every iteration.
@@ -77,9 +80,10 @@ for i = 1:period
     xlabel('Time (s)');
     ylabel('Temperature (Celsius)');
     title('Temperature of Aluminum Bar');
-    legend('Ambient Temperature','Thermocouple 1', 'Thermocouple 2', 'Thermocouple 3', 'Thermocouple 4', 'Heater','NorthWest');
+    legend('Ambient Temperature','Thermocouple 1', 'Thermocouple 2', 'Thermocouple 3', 'Thermocouple 4', 'Heater');
     grid on;
 
+          
     % We pause for our delta-t value, and update the progress bar.
     
     % waitbar(i/period);
@@ -87,7 +91,9 @@ for i = 1:period
     
     % We then pause for our delta-t value, ready to implement this again on
     % the next step!
-    pause(dT);
+    timeElapsed = toc;
+        
+    pause(dT - timeElapsed);
 end
 
 % The final bit of the progress bar, we don't need its kind in here!
