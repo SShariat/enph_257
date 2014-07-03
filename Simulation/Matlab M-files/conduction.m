d46ef0337d9749b7b8a3b45c78090af200749c52
Saman <saman.sj.87gmail.com>
2014-07-03 01:08:21 -0700

    Working Algorithm and Matrix Changes
    None working simulation

--- Simulation/Matlab M-files/conduction.m
+++ Simulation/Matlab M-files/conduction.m
@@ -2,7 +2,7 @@
 function [sim_data]=conduction(specific_heat,kappa,factor,rd_t4)
 
 %Constansts for Test Rod
-elements = 30;
+elements = 40;
 elements_1=elements-1;
 t_amb = 25;
 rod = ones(1,elements)*t_amb;
@@ -11,15 +11,15 @@ cross_area = pi*(diameter_rod/2)^2;
 unit_length = ((30.5-6.1)/elements)/100.0;
 unit_mass = 0.3265;
 surface_a_element = pi*diameter_rod*unit_length;
-d_time = 1;
-number_of_ticks=10800;
+d_time = 0.5;
+number_of_ticks=10800*2;
 P_in = 6.667;
 
 %Initialization of the Data-Recording Vectors
-tc_1_index = 8;
-tc_2_index = 15;
-tc_3_index = 23;
-tc_4_index = 30;
+tc_1_index = 10;
+tc_2_index = 20;
+tc_3_index = 30;
+tc_4_index = 40;
 
 %Initialize Data Tables
 data_time = zeros(number_of_ticks,1);
@@ -33,7 +33,7 @@ time = 0;
 %Real Power into Initial Element
 Q = P_in*factor;
 for count = 1:number_of_ticks
-    rod(1,30) = rd_t4(1,count);
+    rod(1,elements) = rd_t4(1,count);
     data_time(count,1)=time;
     for index=1:elements_1
         %For the first rod block, the energy entering is constant
