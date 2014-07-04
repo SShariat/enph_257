3005c8ab54eaa814ff38a4fca6604163483eb59d
Bryan <bpoteryko@gmail.com>
2014-07-03 18:36:56 -0700

    Revert "Revert "Final Version of the Code""
    This reverts commit 7aadcc1a21c79aac52d8d73a41c122c2e60dda64.

---
+++ Simulation/Matlab M-files/Convection/compare.m
@@ -0,0 +1,4 @@
+function compare(guess,rd)
+plot(rd')
+hold on 
+plot (guess')
--- Simulation/Matlab M-files/epsilon_variables.m
+++ Simulation/Matlab M-files/epsilon_variables.m
@@ -1,6 +1,6 @@
-function [M]=epsilon_variables(c,kf,ec,ekef)
-M = [[(c+ec) kf];[(c-ec) kf]; [c (kf+ekef)]; [c (kf - ekef)]]';
-%M = [[(c+ec) k f];[(c-ec) k f];[c (k+ek) f];[c (k-ek) f];[c k (f+ef)];[c k (f-ef)]]';
+function [M]=epsilon_variables(c,k,f,ec,ek,ef)
+%M = [[(c+ec) kf];[(c-ec) kf]; [c (kf+ekef)]; [c (kf - ekef)]]';
+M = [[(c+ec) k f];[(c-ec) k f];[c (k+ek) f];[c (k-ek) f];[c k (f+ef)];[c k (f-ef)]]';
 % M = num2cell(M);
 
 end
